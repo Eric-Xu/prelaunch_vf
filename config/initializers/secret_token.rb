8729635e29c91c3d65845f8c1891321e6cec826e
Eric-Xu <e.xulander@gmail.com>
2012-12-06 12:56:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrelaunchVf::Application.config.secret_token = '980da08c98077d82adefa0edea850b6a19577d52d9b9332c7dd17bee4c84acbfe7d2eaf0372a5e27713e6c36520ac8a7a567f15344147727a2b18318fa4cd004'
