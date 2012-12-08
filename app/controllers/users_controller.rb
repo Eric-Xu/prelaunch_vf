c7cf5ea02fe33a433e41ab8172fe0ac051707bcc
Eric-Xu <e.xulander@gmail.com>
2012-12-07 21:23:23 -0500

    Add signup error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+      render 'thank_you'
+    else
+      render 'sign_up'
+    end
+  end
+
   def sign_in
   end
 
